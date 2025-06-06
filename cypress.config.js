01306e8ad12b367fae09dabdcff74f2dad440066
Maria Eduarda <eduarda.lem049@gmail.com>
2025-06-06 11:43:29 -0300

    configuração do cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({ // CONFIGURANDO A ALTURA E LARGURA DO NAVEGADOR
   viewportHeight: 880,
   viewportWidth: 1280,
-  e2e: {}
+  e2e: {},
+  projectId: "o3g46r"
 })
